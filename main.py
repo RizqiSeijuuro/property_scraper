05d0b4b81df41fd8b8c5673de2eab250ec34cb50
RizqiSeijuuro <rizqiansyah52@gmail.com>
2025-06-12 17:45:23 +0000

    add: sitemap scraping endpoint

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from property-scraper!")
+import asyncio
+import os
+
+from fastapi import FastAPI, status
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
+from functions.crawler.sitemap_crawler import SitemapCrawler
+
+load_dotenv()
+app = FastAPI()
 
 
-if __name__ == "__main__":
-    main()
+class SitemapURL(BaseModel):
+    """Model for scraping a new sitemap."""
+    sitemap_url: str
+
+
+@app.post("/sitemap", status_code=status.HTTP_201_CREATED)
+def scrape_sitemap(data: SitemapURL):
+    """Scrape url list from sitemap URL."""
+
+    proxy_urls = [os.environ["APIFY_PROXY"]]
+
+    crawler = SitemapCrawler(proxy_urls=proxy_urls)
+    df_post_urls, sitemap_metadata = asyncio.run(crawler.crawl_sitemap(data.sitemap_url))
+    print(sitemap_metadata)
+
+    return df_post_urls.to_json(orient="records")
